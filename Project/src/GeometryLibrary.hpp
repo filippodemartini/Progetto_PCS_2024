db439082c6722435f3a0f50ebac4d5a3dd26d2a1
GabrieleCarnabuci <s295510@studenti.polito.it>
2024-05-15 11:16:33 +0200

    lettura file e baricentro

---
+++ Project/src/GeometryLibrary.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "Eigen/Eigen"
+
+using namespace std;
+using namespace Eigen;
+
+namespace GeometryLibrary {
+
+struct GeometryDFN
+{
+    unsigned int Number_Fractures = 0;
+    vector<unsigned int> Fractures_Id = {};
+    vector<unsigned int> Fractures_Number_Vertices = {};
+    map<unsigned int, vector<Vector3d>> Fractures_Vertices = {};
+
+    unsigned int Number_Traces = 0;
+    vector<unsigned int> Traces_Id = {};
+    vector<Vector2i> Traces_Fractures_Id;
+    map<unsigned int, array<Vector3d,2>> Traces_Coordinates;
+    bool Tips;
+};
+
+}
